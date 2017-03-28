16952ac85a893e51c58c54c409461abe61e04408
Alberto Mariscal <yuniors87@gmail.com>
2017-03-27 21:26:30 -0400

    2

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla nam voluptas beatae consequatur ipsa. Placeat praesentium, perspiciatis nemo atque illum quae, tempora sapiente molestias deserunt necessitatibus mollitia aliquid reprehenderit provident.</p>
       </div>
     );
   }
