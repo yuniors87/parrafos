26b53e14f5ffc344d0f961bc059e28312f0ffb2c
Alberto Mariscal <yuniors87@gmail.com>
2017-03-27 21:30:59 -0400

    3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla nam voluptas beatae consequatur ipsa. Placeat praesentium, perspiciatis nemo atque illum quae, tempora sapiente molestias deserunt necessitatibus mollitia aliquid reprehenderit provident.</p>
+      <div className='App'>
+        Hola mundo
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
